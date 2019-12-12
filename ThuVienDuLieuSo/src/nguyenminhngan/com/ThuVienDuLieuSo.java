774c17dcc59885f49f30c2f90fd5b406115b534d
NGAN <NGAN@ngmhngan>
2019-12-12 19:46:48 +0700

    qtest

--- ThuVienDuLieuSo/src/nguyenminhngan/com/ThuVienDuLieuSo.java
+++ ThuVienDuLieuSo/src/nguyenminhngan/com/ThuVienDuLieuSo.java
@@ -8,16 +8,16 @@ public class ThuVienDuLieuSo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double x=1239646542;
+		double x=1111111/3;
 		int y=3/18;
 		System.out.println(x);
 		System.out.println(y);
 		DecimalFormat dcf = new DecimalFormat("#,###");
+		DecimalFormat sothapphan = new DecimalFormat("#.##");
 		System.out.println(dcf.format(x));
 		DecimalFormatSymbols dcsf= new DecimalFormatSymbols(Locale.getDefault());
 		dcsf.setGroupingSeparator('.');
 		dcf.setDecimalFormatSymbols(dcsf);
-		System.out.println(dcf.format(x));
 	}
 
 }
