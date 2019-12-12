8a86a0a76c1325b21fbdff99e640a498987cf1ee
NGAN <NGAN@ngmhngan>
2019-12-12 19:27:15 +0700

    test

---
+++ ThuVienDuLieuSo/src/nguyenminhngan/com/ThuVienDuLieuSo.java
@@ -0,0 +1,23 @@
+package nguyenminhngan.com;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class ThuVienDuLieuSo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x=1239646542;
+		int y=3/18;
+		System.out.println(x);
+		System.out.println(y);
+		DecimalFormat dcf = new DecimalFormat("#,###");
+		System.out.println(dcf.format(x));
+		DecimalFormatSymbols dcsf= new DecimalFormatSymbols(Locale.getDefault());
+		dcsf.setGroupingSeparator('.');
+		dcf.setDecimalFormatSymbols(dcsf);
+		System.out.println(dcf.format(x));
+	}
+
+}
